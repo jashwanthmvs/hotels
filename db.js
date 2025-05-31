ee7d3edc9c91150017b6725601f155c0f33b044e
JashwanthMarisetty <jashwanthmarisetty217@gmail.com>
2025-05-31 16:25:11 +0530

    Shifted mongodb to online server

--- db.js
+++ db.js
@@ -1,24 +1,24 @@
 const mongoose = require("mongoose");
+require("dotenv").config(); // Load environment variables from .env file
 
-const mongoURL = 'mongodb://localhost:27017/hotelDB';
+// const mongoURL = "mongodb://localhost:27017/hotelDB";
+// const mongoURL = process.env.MONGODB_URL_LOCAL
+const mongoDBURL = process.env.MONGODB_URL || process.env.MONGODB_URL_LOCAL;
 
-mongoose.connect(mongoURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(mongoDBURL);
 
 const db = mongoose.connection;
 
-db.on('connected', () => {
-    console.log('Connected to MongoDB');
+db.on("connected", () => {
+  console.log("Connected to MongoDB");
 });
 
-db.on('error', (err) => {
-    console.error('Error connecting to MongoDB:', err);
-}); 
+db.on("error", (err) => {
+  console.error("Error connecting to MongoDB:", err);
+});
 
-db.on('disconnected', () => {
-    console.log('Disconnected from MongoDB');
+db.on("disconnected", () => {
+  console.log("Disconnected from MongoDB");
 });
 
 module.exports = db;
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+require("dotenv").config(); // Load environment variables from .env file
 
 const MenuItem = require("./models/menu");
 
 app.use(express.json()); // To parse JSON data in request bodies (e.g., from POST requests)
 app.use(express.urlencoded({ extended: true })); // To parse URL-encoded data (e.g., form submissions)
+
+const PORT = process.env.PORT || 3000; // Use the PORT from environment variables or default to 3000
 
 app.get("/", (req, res) => {
   res.send("Welcome to my hotel ");
@@ -17,6 +20,7 @@ app.use("/menu", menuRoutes);
 const personRoutes = require("./routes/personRoutes"); // Import the person routes from personRoutes.js
 app.use("/person", personRoutes); // Use the person routes defined in personRoutes.js
 
-app.listen(3000, () => {
+
+app.listen(PORT, () => {
   console.log("Server is running on port 3000");
 });
